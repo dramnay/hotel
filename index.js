267a7345f70159f1f33a5d26f8380e05ec35d956
dramnay <deepanshu.ramnay@gmail.com>
2023-06-05 12:32:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+const dbConnect = require("./config/dbUtils");
+dbConnect.initDB();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log("Hello from middleware");
+    next();
+});
+
+process.on("SIGINT", () => {
+    dbConnect.disconnectDB();
+    console.log("Closing server");
+    process.exit();
+});
+
+process.on("exit", () => {
+    console.log("Server closed");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
